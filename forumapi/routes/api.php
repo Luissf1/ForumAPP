265f6b884556efd80937cf479859f18636a4a86e
luissilvasf <luis.silva1252@gmail.com>
2024-01-31 14:59:33 -0800

    post and get

--- forumapi/routes/api.php
+++ forumapi/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +24,5 @@ Route::get('/test',function(){
         'message' => 'Api is working'
     ],200);
 });
+
+Route::post('register',[AuthenticationController::class,'register']);
