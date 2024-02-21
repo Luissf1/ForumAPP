2dc97a14402b5b74500c386b777da3d70436fee0
luissilvasf <luis.silva1252@gmail.com>
2024-02-21 13:40:00 -0800

    Functions

--- forumapi/app/Models/Feed.php
+++ forumapi/app/Models/Feed.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Feed extends Model
 {
@@ -15,7 +16,13 @@ class Feed extends Model
         'content'
     ];
 
-    public function user():BelongsTo{
+    public function user(): BelongsTo
+    {
         return $this->belongsTo(User::class);
+    }
+
+    public function likes(): HasMany
+    {
+        return $this->hasMany(Like::class);
     }
 }
