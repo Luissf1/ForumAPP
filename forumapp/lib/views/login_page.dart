e5c23c7237dcd7333520ef1447cbb0d1db1cd7bd
Luis Adrian Silva Reyes <54382882+Luissf1@users.noreply.github.com>
2024-06-11 13:38:37 -0700

    update

--- forumapp/lib/views/login_page.dart
+++ forumapp/lib/views/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forumapp/controllers/authentication.dart';
 import 'package:forumapp/views/register_page.dart';
 import 'package:forumapp/views/widgets/input_widget.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -12,8 +13,10 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final AuthenticationController _authenticationController =
+      Get.put(AuthenticationController());
 
   @override
   Widget build(BuildContext context) {
@@ -21,21 +24,25 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20.0,
+          ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
                 'Login Page',
-                style: GoogleFonts.poppins(fontSize: size * 0.080),
+                style: GoogleFonts.poppins(
+                  fontSize: size * 0.080,
+                ),
               ),
               const SizedBox(
                 height: 30,
               ),
               InputWidget(
-                hintText: 'Email',
+                hintText: 'Username',
                 obscureText: false,
-                controller: _emailController,
+                controller: _usernameController,
               ),
               const SizedBox(
                 height: 20,
@@ -52,16 +59,29 @@ class _LoginPageState extends State<LoginPage> {
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.black,
                   elevation: 0,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                ),
-                onPressed: () {},
-                child: Text(
-                  'Login',
-                  style: GoogleFonts.poppins(
-                    fontSize: size * 0.040,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 50,
+                    vertical: 15,
                   ),
                 ),
+                onPressed: () async {
+                  await _authenticationController.login(
+                    username: _usernameController.text.trim(),
+                    password: _passwordController.text.trim(),
+                  );
+                },
+                child: Obx(() {
+                  return _authenticationController.isLoading.value
+                      ? const CircularProgressIndicator(
+                          color: Colors.white,
+                        )
+                      : Text(
+                          'Login',
+                          style: GoogleFonts.poppins(
+                            fontSize: size * 0.040,
+                          ),
+                        );
+                }),
               ),
               const SizedBox(
                 height: 20,
