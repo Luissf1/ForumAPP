a8624e5036a40b816f95b2c55d4a53a545004e5b
Luis Adrian Silva Reyes <54382882+Luissf1@users.noreply.github.com>
2024-06-18 13:35:57 -0700

    update

--- forumapp/lib/views/widgets/post_data.dart
+++ forumapp/lib/views/widgets/post_data.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:forumapp/controllers/post_controller.dart';
 import 'package:forumapp/models/post_model.dart';
 import 'package:forumapp/views/post_details.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class PostData extends StatelessWidget {
+class PostData extends StatefulWidget {
   const PostData({
     super.key,
     required this.post,
@@ -13,39 +14,63 @@ class PostData extends StatelessWidget {
   final PostModel post;
 
   @override
+  State<PostData> createState() => _PostDataState();
+}
+
+class _PostDataState extends State<PostData> {
+  final PostController _postController = Get.put(PostController());
+  bool likedPost = true;
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
       width: double.infinity,
       decoration: BoxDecoration(
-          color: Colors.grey[200], borderRadius: BorderRadius.circular(10)),
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(10),
+      ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(post.user!.name!, style: GoogleFonts.poppins()),
-          Text(post.user!.email!,
-              style: GoogleFonts.poppins(
-                fontSize: 10,
-              )),
+          Text(
+            widget.post.user!.name!,
+            style: GoogleFonts.poppins(),
+          ),
+          Text(
+            widget.post.user!.email!,
+            style: GoogleFonts.poppins(
+              fontSize: 10,
+            ),
+          ),
           const SizedBox(
             height: 10,
           ),
           Text(
-            post.content!,
+            widget.post.content!,
           ),
           Row(
             children: [
               IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.thumb_up),
+                onPressed: () async {
+                  await _postController.likeAndDislike(widget.post.id);
+                  _postController.getAllPosts();
+                },
+                icon: Icon(
+                  Icons.thumb_up,
+                  color: widget.post.liked! ? Colors.blue : Colors.black,
+                ),
               ),
               IconButton(
-                  onPressed: () {
-                    Get.to(() => PostDetails(
-                          post: post,
-                        ));
-                  },
-                  icon: const Icon(Icons.message))
+                onPressed: () {
+                  Get.to(
+                    () => PostDetails(
+                      post: widget.post,
+                    ),
+                  );
+                },
+                icon: Icon(Icons.message),
+              ),
             ],
           ),
         ],
